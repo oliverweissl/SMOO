125a8fa0fe812dc5707adf25f5625fbbb8afa731
Oliver Weissl <oliver.weissl@outlook.com>
2026-01-14 15:26:22 +0100

    Upd Formatting

--- src/optimizer/_optimizer.py
+++ src/optimizer/_optimizer.py
@@ -87,17 +87,16 @@ class Optimizer(ABC):
 
         i = 0
         while i < indices.shape[0]:
-            on_pareto = np.ones(indices.shape[0], dtype=bool)
+            on_pareto: NDArray = np.ones(indices.shape[0], dtype=bool)
 
             cur = metrics[indices[i]]
-            rest = metrics[indices[i + 1:]]
+            rest = metrics[indices[i + 1 :]]
 
-            dominated_by_cur = (
-                    (rest >= cur).all(axis=1)
-                    & (rest > cur).any(axis=1)
+            dominated_by_cur = (rest >= cur).all(axis=1) & (rest > cur).any(
+                axis=1
             )  # minimization only
 
-            on_pareto[i + 1:] = ~dominated_by_cur
+            on_pareto[i + 1 :] = ~dominated_by_cur
             indices = indices[on_pareto]
 
             i += 1
